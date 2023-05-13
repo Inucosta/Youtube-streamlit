c48f28f0ed45b1a85863d45f8e6983ce4877db92
Inucosta <parallelryo@iclod.com>
2023-05-13 11:20:15 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+import time
+
+st.title('Streamlit 超入門')
+
+st.write('ProgressBar')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)    #0->int, 0.0->float
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+'Done!!!' 
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラムです．')
+    
+expander1 = st.expander('問い合わせ1')
+expander1.write('問い合わせ1回答')
+expander2 = st.expander('問い合わせ2')
+expander2.write('問い合わせ2回答')
+expander3 = st.expander('問い合わせ3')
+expander3.write('問い合わせ3回答')
+# option = st.sidebar.text_input('あなたの趣味を教えてください．')
+# condition = st.sidebar.slider('あなたの今の調子は？', 0, 100, 50)  #min, max, center
+# 'あなたの趣味は', option, 'です．'
+# 'コンディション', condition
+# option = st.selectbox(
+#     'あなたが好きな数字を教えてください',
+#     list(range(1,11))
+# )
+# 
+# 'あなたの好きな数字は，', option, 'です．'
+# if st.checkbox('Show Image'):
+#     img =Image.open('C:\\Users\\paral\\Documents\\ryo\\CAD\\photo\\ウソハチ.png')
+#     st.image(img, caption='Usohati', use_column_width=True)
